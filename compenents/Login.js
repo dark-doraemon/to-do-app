3ccd22d3c87dd992ee68f576801aea327aa039a8
dark-doraemon <tuandinhbi75@gmail.com>
2023-04-02 21:29:55 +0700

    test

--- compenents/Login.js
+++ compenents/Login.js
@@ -8,10 +8,10 @@ export const Login = ({navigation}) =>{
     const [password,setPassword] = useState('');
 
     const handleSignIn = () => {
-        if(gmail == '' || password == '')
-        {
-            alert("Tài khoản hoặc mật khẩu không được để trống");
-        }
+        // if(gmail == '' || password == '')
+        // {
+        //     alert("Tài khoản hoặc mật khẩu không được để trống");
+        // }
         const auth = getAuth(auth); //lấy đối tượng Auth của firebase Authentication thông qua app
         signInWithEmailAndPassword(auth,gmail,password)//kiểm tra gmail và password trong firebase thông qua auth
         .then((userCredential) =>{
